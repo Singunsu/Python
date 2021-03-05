70044c1d771b6e32320cedf58b2e24e1d20fc92d
Singunsu <76992660+Singunsu@users.noreply.github.com>
2021-03-05 15:59:27 +0900

    dddd
    ddddd

---
+++ 학번.py
@@ -0,0 +1,34 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("201887004 신건수");
+201887004 신건수
+>>> input ("이름을 입력하세요 : ");
+이름을 입력하세요 : 신건수
+'신건수'
+>>> '신건수'
+'신건수'
+>>> 1+2
+3
+>>> 78/5
+15.6
+>>> name = input ("이름 : ");
+이름 : 신건수
+>>> print(1+2);
+3
+>>> name
+'신건수'
+>>> stdNum = input("학번 : ");
+학번 : 201887004 신건수
+>>> stdName = input("이름 : ");
+이름 : 신건수
+>>> print("당신의 학번은 ", stdNum, "이고 이름은 ", stdNum, "입니다.");
+당신의 학번은  201887004 신건수 이고 이름은  201887004 신건수 입니다.
+>>> >>> print("당신의 학번은 ", stdNum, "이고 이름은 ", stdName, "입니다.");
+SyntaxError: invalid syntax
+>>> print("당신의 학번은 ", stdNum, "이고 이름은 ", stdName, "입니다.");
+당신의 학번은  201887004 신건수 이고 이름은  신건수 입니다.
+>>> stdNum = input("학번 : ");
+학번 : 201887004
+>>> print("당신의 학번은 ", stdNum, "이고 이름은 ", stdName, "입니다.");
+당신의 학번은  201887004 이고 이름은  신건수 입니다.
+>>> 
