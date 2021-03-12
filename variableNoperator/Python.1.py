f97250eab7dee7904d4649d5eaec027b5ece83c9
Singunsu <76992660+Singunsu@users.noreply.github.com>
2021-03-12 16:06:45 +0900

    asdasd
    asdasd

---
+++ variableNoperator/Python.1.py
@@ -0,0 +1,17 @@
+# 문자열 입력
+a = input("첫 번째 숫자 : ");
+b = input("두 번째 숫자 : ");
+print(a + b);
+print("Hello" + "python");
+
+# 두 정수를 입력받아 사칙연산 결과를 출력
+a = int(input("첫 번째 숫자 : ");
+b = int(input("두 번째 숫자 : ");
+print(a,"+",b,"=",a+b);
+print(a,"-",b,"=",a-b);
+print(a,"*",b,"=",a*b);
+print(a,"/",b,"=",a/b);
+
+c = a + b
+print(a , " + " , b , " = " , c);
+
